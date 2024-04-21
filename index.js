bb23f4a7e6432efa4c010280d8d16bc356c1e040
Nafis Ahamed <nafisahamed14@gmail.com>
2024-04-21 23:25:58 +0600

    deleted

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5005
 const cors = require('cors')
 app.use(cors())
@@ -32,11 +32,24 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     const coffeeCollection = client.db('coffeeDB').collection('coffee')
+    app.get('/coffee',async(req,res)=>{
+       const cursor = coffeeCollection.find()
+       const result = await cursor.toArray()
+       res.send(result)
+    })
+    
     app.post('/coffee',async(req,res)=>{
        const newCoffee = req.body;
        console.log(newCoffee);
        const result = await coffeeCollection.insertOne(newCoffee)
        res.send(newCoffee)
+    })
+
+    app.delete('/coffee/:id',async(req,res)=>{
+        const id = req.params.id
+        const query = {_id : new ObjectId(id)}
+        const result = await coffeeCollection.deleteOne(query)
+        res.send(result)
     })
 
     await client.db("admin").command({ ping: 1 });
