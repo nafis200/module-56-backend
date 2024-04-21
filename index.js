a2f74ea57b93d375b5a2fd06df22593155e569cd
Nafis Ahamed <nafisahamed14@gmail.com>
2024-04-21 22:00:47 +0600

    sweet alert

--- index.js
+++ index.js
@@ -31,11 +31,19 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+    const coffeeCollection = client.db('coffeeDB').collection('coffee')
+    app.post('/coffee',async(req,res)=>{
+       const newCoffee = req.body;
+       console.log(newCoffee);
+       const result = await coffeeCollection.insertOne(newCoffee)
+       res.send(newCoffee)
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
